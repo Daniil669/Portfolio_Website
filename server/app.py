ea70f35a1139fe07f898633862aa1f223d4f2674
Daniil Freeman <daniil.freeman669@gmail.com>
2025-06-29 15:36:21 +0300

    cors and a bit of async

--- server/app.py
+++ server/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import flask_cors
 from routes.info_routes import info_bp
 from routes.github_routes import github_bp
 from routes.contact_routes import contact_bp
 
 app = Flask(__name__)
+
+cors = flask_cors.CORS(app, resources={r"/api/*": {"origins": ["http://localhost:8000"]}}) #change localhost
+
 app.register_blueprint(info_bp, url_prefix='/api/info')
 app.register_blueprint(github_bp, url_prefix='/api/github')
 app.register_blueprint(contact_bp, url_prefix='/api/contact')
--- server/utils/github_utils.py
+++ server/utils/github_utils.py
@@ -4,6 +4,7 @@ import dotenv
 import base64
 from bs4 import BeautifulSoup
 import markdown
+import aiohttp
 
 dotenv.load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
