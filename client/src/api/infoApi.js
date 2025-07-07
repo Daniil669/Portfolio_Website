d145da27c7741dcd079a4a3172f3a55b0e893831
Daniil Freeman <daniil.freeman669@gmail.com>
2025-07-07 11:59:34 +0300

    Fetching data for frontend 1:3 ready

--- client/src/api/infoApi.js
+++ client/src/api/infoApi.js
@@ -2,16 +2,64 @@ import axios from 'axios';
 
 const API_BASE = import.meta.env.VITE_API_BASE
 
-const about = () => {
-    axios.get(`${API_BASE}/api/info/about`).
-    then((reponse)=>{
-        const result = reponse.data
-        return result
-    }).catch((error)=>{
+const about_api = async () => {
+    try{
+    const response = await axios.get(`${API_BASE}/api/info/about`)
+    const result = response.data
+    return result
+    }catch(error){
         console.log(error)
-    })
+    }
 }
 
+const about_photo_api = async () => {
+    try {
+    const response = await axios.get(`${API_BASE}/api/info/profile_photo`, {responseType:'blob'})
+    const result = URL.createObjectURL(response.data)
+    return result
+    } catch(error){
+        console.log(error)
+    }
+}
 
+const projects_api = async () => {
+    try{
+    const response = await axios.get(`${API_BASE}/api/info/projects`)
+    const result = response.data
+    return result
+    }catch(error){
+        console.log(error)
+    }
+}
 
-export {about}
+const freelance_project_api = async () => {
+    try {
+        const response = await axios.get(`${API_BASE}/api/info/freelance_project`)
+        const result = response.data
+        return result
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const services_api = async () => {
+    try {
+        const response = await axios.get(`${API_BASE}/api/info/service`)
+        const result = response.data
+        return result
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const cv_api = async () => {
+    try {
+        const response = await axios.get(`${API_BASE}/api/info/cv`, {responseType: 'blob'})
+        const result = URL.createObjectURL(response.data)
+        return result
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {about_api, projects_api, about_photo_api, freelance_project_api, services_api, cv_api}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
-import { about } from "../../api/infoApi";
+import { cv_api } from "../../api/infoApi";
 import { useState, useEffect } from "react";
 
 export default function Home() {
+    const [imgUrl, setImgUrl] = useState(null)
+    const [data, setData] = useState({})
+
     useEffect(()=>{
-        about()
+    const fetchData = async () =>{
+        const data_ = await cv_api()
+        console.log(data_)
+        setData(data_)
+
+    }
+    fetchData()
     }, [])
+
     return (
+        <>
         <h1>Home</h1>
+        <img src={imgUrl} alt="image" />
+        </>
     );
 }
